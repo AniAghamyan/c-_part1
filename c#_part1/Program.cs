98341da5c3a6b5f734182c3d48f47671180f4211
ProBook <ProBook@DESKTOP-RT7SM8C>
2024-10-22 15:57:05 +0400

    Add project files.

---
+++ c#_part1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static bool IsPalindrome(string input)
+    {        
+        string processedString = new string(input.ToLower().ToCharArray());
+        int start = 0;
+        int end = processedString.Length - 1;
+        while (start < end)
+        {
+            if (processedString[start] != processedString[end])
+            {
+                return false; 
+            }
+            start++;
+            end--;
+        }
+
+        return true; 
+    }
+
+    static void Main()
+    {
+        string input = "racecar";  
+        Console.WriteLine($"Input: {input}");
+
+        if (IsPalindrome(input))
+        {
+            Console.WriteLine("Output: True.");
+        }
+        else
+        {
+            Console.WriteLine("Output: False.");
+        }
+    }
+}
